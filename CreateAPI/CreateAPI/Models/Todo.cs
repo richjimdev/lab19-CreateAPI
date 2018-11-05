f4ca81173adaf6e864cc4d741b0fb90f58f06c7a
Richard J <richjim89@gmail.com>
2018-11-04 22:11:58 -0800

    models

---
+++ CreateAPI/CreateAPI/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateAPI.Models
+{
+    public class Todo
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public bool Complete { get; set; }
+    }
+}
--- CreateAPI/CreateAPI/Startup.cs
+++ CreateAPI/CreateAPI/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CreateAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,6 +24,11 @@ namespace CreateAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]);
+            });
+            services.AddMvc().AddXmlDataContractSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
