3fa56f120772325ffee5b1724bcd0af7ffd27218
Richard J <richjim89@gmail.com>
2018-11-04 22:07:42 -0800

    initial scaffolding

---
+++ CreateAPI/CreateAPI/Data/TodoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateAPI.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<TodoList> TodoLists { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# lab19-CreateAPI
+## Lab19-CreatAPI
+
+# Description
+This repo consits of an API that users can use to create and update a todo list.
+
+# Visuals
+
+# Directions
+- Clone repo
+- Open solution using Visual Studio
+- Run the app using IIS
+- Open Postman and send requests to the API to use : (link)
