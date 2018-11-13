6db7c8326428124c718fce8b8d8be80636f3cbac
Richard J <richjim89@gmail.com>
2018-11-12 20:07:42 -0800

    CRUD todo tests

--- CreateAPI/CreateAPITest/UnitTest1.cs
+++ CreateAPI/CreateAPITest/UnitTest1.cs
@@ -38,6 +38,7 @@ namespace CreateAPITest
         [Fact]
         public async void CanCreateATodo()
         {
+            //Arrange
             DbContextOptions<TodoDbContext> options = new DbContextOptionsBuilder<TodoDbContext>()
                 .UseInMemoryDatabase("GetTodoName")
                 .Options;
@@ -50,9 +51,69 @@ namespace CreateAPITest
                 context.Todos.Add(task);
                 context.SaveChanges();
 
+                //Act
                 var taskTitle = await context.Todos.FirstOrDefaultAsync(x => x.Title == task.Title);
 
+                //Assert
                 Assert.Equal("todoTest", taskTitle.Title);
+            }
+        }
+
+        [Fact]
+        public async void CanUpdateATodo()
+        {
+            //Arrange
+            DbContextOptions<TodoDbContext> options =
+                new DbContextOptionsBuilder<TodoDbContext>()
+                .UseInMemoryDatabase("GetTodoName")
+                .Options;
+
+            using (TodoDbContext context = new TodoDbContext(options))
+            {
+                Todo task = new Todo();
+                task.Title = "testTodo";
+
+                context.Todos.Add(task);
+                context.SaveChanges();
+
+                //Act
+                task.Title = "updateTodo";
+
+                context.Todos.Update(task);
+                context.SaveChanges();
+
+                var taskTitle = await context.Todos.FirstOrDefaultAsync(x => x.Title == task.Title);
+
+                //Assert
+                Assert.Equal("updateTodo", taskTitle.Title);
+            }
+        }
+
+        [Fact]
+        public async void CanDeleteATodo()
+        {
+            //Arrange
+            DbContextOptions<TodoDbContext> options =
+                new DbContextOptionsBuilder<TodoDbContext>()
+                .UseInMemoryDatabase("GetTodoName")
+                .Options;
+
+            using (TodoDbContext context = new TodoDbContext(options))
+            {
+                Todo task = new Todo();
+                task.Title = "testTodo";
+
+                context.Todos.Add(task);
+                context.SaveChanges();
+
+                //Act
+                context.Todos.Remove(task);
+                context.SaveChanges();
+
+                var taskTitle = await context.Todos.ToListAsync();
+
+                //Assert
+                Assert.DoesNotContain(task, taskTitle);
             }
         }
     }
